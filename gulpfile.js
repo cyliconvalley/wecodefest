9fe2e1dd784200a575764c2151115c9a3920c234
Roberto González <roberto.gonzalez@vitalinnova.com>
2017-11-10 17:43:16 +0100

    Autoprefixer added, copys and design fixes

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var htmlmin = require('gulp-htmlmin');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
-  return gulp.src('./src/sass/style.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./'));
+	return gulp.src('./src/sass/style.scss')
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(gulp.dest('./'));
 });
 
 gulp.task('minify', function() {
-  return gulp.src('./src/index.html')
-    .pipe(htmlmin({collapseWhitespace: true, minifyJS: true}))
-    .pipe(gulp.dest('./'));
+	return gulp.src('./src/index.html')
+	.pipe(htmlmin({collapseWhitespace: true, minifyJS: true}))
+	.pipe(gulp.dest('./'));
 });
